2faae000b5b10d920615bda7e32040aabf48f318
sagewall <sagemaps@gmail.com>
2017-04-07 11:18:42 -0600

    Remove post, put and delete api endpoints

--- routes/api.js
+++ routes/api.js
@@ -15,98 +15,29 @@ router.get('/', (req, res) => {
   res.send('gis-data-catalog api');
 });
 
-router.route('/datasets')
-  .post((req, res) => {
-    const dataset = new Dataset();
-    dataset.name = req.body.name;
-    dataset.date = req.body.date;
-    dataset.about = req.body.about;
-    dataset.tags = req.body.tags;
-    dataset.downloadUrl = req.body.downloadUrl;
-    dataset.dynamicMapLayer = req.body.dynamicMapLayer;
-    dataset.lat = req.body.lat;
-    dataset.lon = req.body.lon;
-    dataset.zoom = req.body.zoom;
+router.get('/datasets', (req, res) => {
+  Dataset.find((err, datasets) => {
+    if (err) {
+      res.send(err);
+    }
 
-    dataset.save((err) => {
+    res.json(datasets);
+  })
+});
+
+router.get('/datasets/:_id', (req, res) => {
+  if (req.params._id.match(/^[0-9a-fA-F]{24}$/)) {
+    Dataset.findById(req.params._id, (err, dataset) => {
       if (err) {
         res.send(err);
       }
 
-      res.json({message: 'Dataset created'});
+      res.json(dataset);
     })
-  })
-
-  .get((req, res) => {
-    Dataset.find((err, datasets) => {
-      if (err) {
-        res.send(err);
-      }
-
-      res.json(datasets);
-    })
-  });
-
-router.route('/datasets/:_id')
-  .get((req, res) => {
-    if (req.params._id.match(/^[0-9a-fA-F]{24}$/)) {
-      Dataset.findById(req.params._id, (err, dataset) => {
-        if (err) {
-          res.send(err);
-        }
-
-        res.json(dataset);
-      })
-    } else {
-      res.status(403).send({ error: 'Forbidden' });
-    }
-  })
-
-  .put((req, res) => {
-    if (req.params._id.match(/^[0-9a-fA-F]{24}$/)) {
-      Dataset.findById(req.params._id, (err, dataset) => {
-        if (err) {
-          res.send(err);
-        }
-
-        dataset.name = req.body.name;
-        dataset.date = req.body.date;
-        dataset.about = req.body.about;
-        dataset.tags = req.body.tags;
-        dataset.downloadUrl = req.body.downloadUrl;
-        dataset.dynamicMapLayer = req.body.dynamicMapLayer;
-        dataset.lat = req.body.lat;
-        dataset.lon = req.body.lon;
-        dataset.zoom = req.body.zoom;
-
-        dataset.save((err) => {
-          if (err) {
-            res.send(err);
-          }
-
-          res.json({message: 'Dataset updated'})
-        })
-      })
-    } else {
-      res.status(403).send({ error: 'Forbidden' });
-    }
-  })
-
-  .delete((req, res) => {
-    if (req.params._id.match(/^[0-9a-fA-F]{24}$/)) {
-      Dataset.remove({
-        _id: req.params._id
-      }, (err, dataset) => {
-        if (err) {
-          res.send(err);
-        }
-
-        res.json({message: 'Successfully deleted'});
-      })
-    } else {
-      res.status(403).send({ error: 'Forbidden' });
-    }
-  });
+  } else {
+    res.status(403).send({ error: 'Forbidden' });
+  }
+});
 
 router.get('/datasets/search/:term', (req, res) => {
   const searchExpression = new RegExp(req.params.term, 'i');
