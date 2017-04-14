9b30005bb967409fa9f8b7c0fdd95e6bc8c2f4d3
sagewall <sagemaps@gmail.com>
2017-04-14 13:37:47 -0600

    remove cors from production environment

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const helmet = require('helmet');
-const cors = require('cors');
 
 const api = require(path.join(__dirname, 'routes/api'));
+
 const certbotResponse = process.env.CERTBOTRESPONSE;
 
 const app = express();
+
+if (process.env.NODE_ENV !== 'production') {
+  const cors = require('cors');
+  app.use(cors());
+}
+
 app.use(helmet());
-app.use(cors());
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/.well-known/acme-challenge/:content', (req, res) => {
